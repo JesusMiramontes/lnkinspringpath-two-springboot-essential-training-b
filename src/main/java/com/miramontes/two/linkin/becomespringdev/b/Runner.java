6cc18cfcefc381a274c59b5334eb0ad377acde14
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-12-31 11:15:09 -0600

    Logger

--- src/main/java/com/miramontes/two/linkin/becomespringdev/b/Runner.java
+++ src/main/java/com/miramontes/two/linkin/becomespringdev/b/Runner.java
@@ -1,6 +1,8 @@
 package com.miramontes.two.linkin.becomespringdev.b;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Component;
 public class Runner implements CommandLineRunner {
     private final RabbitTemplate rabbitTemplate;
     private final ObjectMapper mapper;
+    private static final Logger LOGGER = LoggerFactory.getLogger(Runner.class);
 
     @Autowired
     private ConfigurableApplicationContext context;
@@ -28,5 +31,6 @@ public class Runner implements CommandLineRunner {
         payload.setModel("ROOM");
         rabbitTemplate.convertAndSend("operations", "landon.rooms.cleaner", mapper.writeValueAsString(payload));
         context.close();
+        LOGGER.info("Successfully sent payload. Room " + index + " needs to be cleaned.");
     }
 }
